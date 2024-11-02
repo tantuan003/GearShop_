fe59c30996b95ec37eb60c16bae4d8be875f2e79
tantuan003 <tantuan0162@gmail.com>
2024-11-02 14:48:01 +0700

    commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,4 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
 
 import express from 'express'
 import { mapOrder } from '~/utils/sorts.js'
@@ -23,10 +19,10 @@ app.get('/', (req, res) => {
     ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
     'id'
   ))
-  res.end('<h1>Hello World!</h1><hr>')
+  res.end('<h1>Nguyen Tan Tuan</h1><hr>')
 })
 
 app.listen(port, hostname, () => {
   // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
+  console.log(`Hello Nguyen Tan Tuan, I am running at ${ hostname }:${ port }/`)
 })
